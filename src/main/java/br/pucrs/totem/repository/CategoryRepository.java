bb48b5b122eace2c0040d1d4c4226cba9a97d778
Ashiley Bianca <ashbycontato@gmail.com>
2025-10-20 23:30:22 -0300

    Add order by name asc

--- src/main/java/br/pucrs/totem/repository/CategoryRepository.java
+++ src/main/java/br/pucrs/totem/repository/CategoryRepository.java
@@ -1,10 +1,15 @@
 package br.pucrs.totem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import br.pucrs.totem.entity.Category;
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     
     Optional<Category> findByName(String name);
+    
+    @Query("SELECT c FROM Category c ORDER BY c.name ASC")
+    List<Category> findAllOrderByNameAsc();
 }
--- src/main/java/br/pucrs/totem/service/CategoryService.java
+++ src/main/java/br/pucrs/totem/service/CategoryService.java
@@ -22,7 +22,7 @@ public class CategoryService {
     }
 
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        return categoryRepository.findAllOrderByNameAsc();
     }
     
     public Optional<Category> findCategoryById(Long id) {
