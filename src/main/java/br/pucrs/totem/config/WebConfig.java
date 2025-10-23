1dafbbe7461c2eb0c4a15701506fd714b7694df5
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2025-10-23 15:54:51 -0300

    fix: update CORS configuration to use allowedOrigins instead of allowedOriginPatterns and disable credentials

--- src/main/java/br/pucrs/totem/config/WebConfig.java
+++ src/main/java/br/pucrs/totem/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3600);
     }
 }
